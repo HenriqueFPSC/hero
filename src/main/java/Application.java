99e61fccacf3e3353a907e725caec9be1c0f0012
Henrique Caridade <up202108817@up.pt>
2022-10-02 19:11:00 +0100

    Added Arena class and Walls

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,5 @@
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,6 +8,8 @@ import com.googlecode.lanterna.input.KeyStroke;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -22,10 +24,12 @@ public class Application {
 
 class Game {
     private Screen screen;
-    private Hero hero = new Hero(10, 10);
+    private Arena arena;
 
     public Game () throws IOException {
-        TerminalSize terminalSize = new TerminalSize(40,20);
+        int width = 40, height = 20;
+        TerminalSize terminalSize = new TerminalSize(width,height);
+        arena = new Arena(width, height);
         DefaultTerminalFactory terminalFactory = new
                 DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
@@ -42,15 +46,71 @@ class Game {
             KeyStroke key = screen.readInput();
             isRunning = processKey(key);
         }
+        screen.close();
     }
 
     private void draw() throws IOException {
         screen.clear();
-        hero.draw(screen);
+        arena.draw(screen.newTextGraphics());
         screen.refresh();
     }
 
-    private boolean processKey(KeyStroke key) throws IOException {
+    private boolean processKey(KeyStroke key){
+       return arena.processKey(key);
+    }
+}
+
+class Hero {
+    private Position pos;
+    public Hero(int x, int y) { pos = new Position(x, y); }
+
+    public Position getPos(){ return pos; }
+    public void setPos(Position pos){ this.pos = pos; }
+
+    public Position moveUp() { return new Position(pos.getX(), pos.getY() - 1); }
+    public Position moveDown() { return new Position(pos.getX(), pos.getY() + 1); }
+    public Position moveLeft() { return new Position(pos.getX() - 1, pos.getY()); }
+    public Position moveRight() { return new Position(pos.getX() + 1, pos.getY()); }
+
+    public void draw(TextGraphics graphics){
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(pos.getX(), pos.getY()), "X");
+    }
+}
+
+class Position {
+    private int x, y;
+    public Position(int x, int y){
+        this.x = x; this.y = y;
+    }
+    public int getX() { return x; }
+    public void setX(int x) { this.x = x; }
+
+    public int getY() { return y; }
+    public void setY(int y) { this.y = y; }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        Position p = (Position) o;
+        return x == p.getX() && y == p.getY();
+    }
+}
+
+class Arena {
+    private int width, height;
+    private List<Wall> walls;
+    private Hero hero = new Hero(10, 10);
+    public Arena(int width, int height){
+        this.width = width;
+        this.height = height;
+        walls = createWalls();
+    }
+
+    public boolean processKey(KeyStroke key){
         switch (key.getKeyType()) {
             case ArrowUp:
                 moveHero(hero.moveUp());
@@ -65,46 +125,59 @@ class Game {
                 moveHero(hero.moveRight());
                 break;
             case Character:
-                if (key.getCharacter() == 'q'){
-                    screen.close();
+                if (key.getCharacter() == 'q')
                     return false;
-                }
                 break;
             case EOF:
                 return false;
         }
         return true;
     }
+    private void moveHero(Position pos){
+        if (canHeroMove(pos))
+            hero.setPos(pos);
+    }
+    private boolean canHeroMove(Position pos){
+        if (pos.getX() < 0) return false;
+        if (pos.getX() >= width) return false;
+        if (pos.getY() < 0) return false;
+        if (pos.getY() >= height) return false;
+        for (Wall wall : walls)
+            if (pos.equals(wall.getPos()))
+                return false;
+        return true;
+    }
+    public void draw(TextGraphics graphics){
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
+        graphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(width, height), ' ');
+        hero.draw(graphics);
+        for (Wall wall : walls) wall.draw(graphics);
+    }
 
-    private void moveHero(Position pos){ hero.setPos(pos); }
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
+    }
 }
 
-class Hero {
+class Wall {
     private Position pos;
-    public Hero() { pos = new Position(10, 10); }
-    public Hero(int x, int y) { pos = new Position(x, y); }
+    public Wall(int x, int y) { pos = new Position(x, y); }
 
     public Position getPos(){ return pos; }
     public void setPos(Position pos){ this.pos = pos; }
 
-    public Position moveUp() { return new Position(pos.getX(), pos.getY() - 1); }
-    public Position moveDown() { return new Position(pos.getX(), pos.getY() + 1); }
-    public Position moveLeft() { return new Position(pos.getX() - 1, pos.getY()); }
-    public Position moveRight() { return new Position(pos.getX() + 1, pos.getY()); }
-
-    public void draw(Screen screen){
-        screen.setCharacter(pos.getX(), pos.getY(), TextCharacter.fromCharacter('X')[0]);
+    public void draw(TextGraphics graphics){
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(pos.getX(), pos.getY()), "O");
     }
-}
-
-class Position {
-    private int x, y;
-    public Position(int x, int y){
-        this.x = x; this.y = y;
-    }
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
-
-    public int getY() { return y; }
-    public void setY(int y) { this.y = y; }
 }
